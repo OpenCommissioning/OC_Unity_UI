122b09e69cf4239ae752a86714b3a4f9335a9f85
Rossi112 <rossjan112@gmail.com>
2025-11-30 12:24:28 +0100

    fix: add multi camera controller functionality

--- Runtime/System/Scripts/Toolbar/CameraSystem.cs
+++ Runtime/System/Scripts/Toolbar/CameraSystem.cs
@@ -1,23 +1,23 @@
 using System.Collections.Generic;
 using System.Linq;
-using Unity.Cinemachine;
 using OC.UI.Panel;
 using UnityEngine;
+using OC.UI.Interactions;
 
 namespace OC.UI.Toolbar
 {
     public class CameraSystem : ToolbarSystemPanel
     {
-        private List<CinemachineVirtualCamera> _cameras;
+        private List<CameraControllerMaster> _cameraControllers;
         private List<ToggleSlide> _toggles = new ();
 
         protected override void AddContent(SubsystemPanel subsystemPanel)
         {
-            _cameras = FindObjectsByType<CinemachineVirtualCamera>(FindObjectsSortMode.InstanceID).ToList();
+            _cameraControllers = FindObjectsByType<CameraControllerMaster>(FindObjectsSortMode.InstanceID).ToList();
 
-            for (var i = 0; i < _cameras.Count; i++)
+            for (var i = 0; i < _cameraControllers.Count; i++)
             {
-                var toggle = new ToggleSlide(_cameras[i].name);
+                var toggle = new ToggleSlide(_cameraControllers[i].name);
                 var index = i;
                 toggle.OnValueChanged += b =>
                 {
@@ -27,22 +27,22 @@ namespace OC.UI.Toolbar
                 subsystemPanel.Add(toggle);
             }
 
-            DisalbeAll();
+            DisableAll();
             SetVirtualCameraState(0, true);
         }
 
         private void SetVirtualCameraState(int index, bool enable)
         {
-            DisalbeAll();
+            DisableAll();
             _toggles[index].SetValueWithoutNotify(true);
-            _cameras[index].Priority = enable ? 10 : 0;
+            _cameraControllers[index].Prioritize();
         }
 
-        private void DisalbeAll()
+        private void DisableAll()
         {
-            foreach (var virtualCamera in _cameras)
+            foreach (var cameraController in _cameraControllers)
             {
-                virtualCamera.Priority = 0;
+                cameraController.Priority(0);
             }
 
             foreach (var toggle in _toggles)
