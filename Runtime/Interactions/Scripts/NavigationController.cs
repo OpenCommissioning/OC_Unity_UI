85ae935af563446ec5769c3604a62120dd63c56b
Rossi112 <rossjan112@gmail.com>
2025-11-30 12:04:56 +0100

    fix: add sensitivity functionality

--- Runtime/Interactions/Scripts/NavigationController.cs
+++ Runtime/Interactions/Scripts/NavigationController.cs
@@ -1,5 +1,4 @@
-using System.Collections.Generic;
-using System.Linq;
+using System.Collections;
 using Unity.Cinemachine;
 using UnityEngine;
 
@@ -7,7 +6,42 @@ namespace OC.UI.Interactions
 {
     public class NavigationController : MonoBehaviour
     {
+        public bool Enabled => _defaultCamera.Priority == 1;
         public float DistanceToPivot => _orbitCameraController.DistanceToPivot;
+        public CinemachineBrain Brain => CinemachineCore.FindPotentialTargetBrain(_defaultCamera);
+        public IPropertyReadOnly<float> RotationGain => _rotationGain;
+        public IPropertyReadOnly<float> ScrollGain => _scrollGain;
+        public IPropertyReadOnly<float> MoveSpeed => _moveSpeed;
+
+        public int MouseSensitivity
+        {
+            get => _mouseSensitivity;
+            set
+            {
+                _mouseSensitivity = value;
+                RefreshSettings();
+            }
+        }
+
+        public int MoveSensitivity
+        {
+            get => _moveSensitivity;
+            set
+            {
+                _moveSensitivity = value;
+                RefreshSettings();
+            }
+        }
+
+        public int ScrollSensitivity
+        {
+            get => _scrollSensitivity;
+            set
+            {
+                _scrollSensitivity = value;
+                RefreshSettings();
+            }
+        }
 
         [Header("State")]
         [SerializeField] private Property<CameraMode> _mode = new (CameraMode.None);
@@ -17,8 +51,27 @@ namespace OC.UI.Interactions
         [SerializeField] private PanCameraController _panCameraController;
         [SerializeField] private FPSCameraController _fpsCameraController;
 
+        [Header("Settings")]
+        [Range(1,10)]
+        [SerializeField]
+        private int _mouseSensitivity = 5;
+        [Range(1,10)]
+        [SerializeField] 
+        private int _moveSensitivity = 5;
+        [Range(1, 10)] 
+        [SerializeField] 
+        private int _scrollSensitivity = 5;
+
         [Header("Testing")]
         [SerializeField] private bool _debug = false;
+
+        private const float ROTATION_GAIN_DEFAULT = 0.5f;
+        private const float SCROLL_GAIN_DEFAULT = 0.02f;
+        private const float MOVE_SPEED_DEFAULT = 1f;
+
+        [SerializeField] private Property<float> _rotationGain = new(0f);
+        [SerializeField] private Property<float> _scrollGain = new(0f);
+        [SerializeField] private Property<float> _moveSpeed = new(0f);
 
         private CursorHandler _cursorHandler;
 
@@ -36,9 +89,21 @@ namespace OC.UI.Interactions
             CinemachineCore.CameraActivatedEvent.RemoveListener(OnCameraActivated);
         }
 
+        private void OnValidate()
+        {
+            RefreshSettings();
+        }
+
         private void OnModeChanged(CameraMode mode)
         {
             _cursorHandler.SetCursor(mode);
+        }
+
+        private void RefreshSettings()
+        {
+            _rotationGain.Value = ROTATION_GAIN_DEFAULT * _mouseSensitivity;
+            _scrollGain.Value = SCROLL_GAIN_DEFAULT * _scrollSensitivity;
+            _moveSpeed.Value = MOVE_SPEED_DEFAULT * _moveSensitivity;
         }
 
         private void OnCameraActivated(ICinemachineCamera.ActivationEventParams args)
